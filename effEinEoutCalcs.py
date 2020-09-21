9fafeebcaf127cd7bdbabf9355ce8e23c7840ced
Gordon <47766574+JDoes@users.noreply.github.com>
2020-09-21 07:08:59 -0700

    Update effEinEoutCalcs.py
    Updated formatting and spacing

--- effEinEoutCalcs.py
+++ effEinEoutCalcs.py
@@ -1,13 +1,16 @@
 #Program to let you select what you want calculated, and then
 #the program asks fro the stuff that is needed to calculate it.
 
-num = str(input("EFF = Eout/Ein * 100%\nWhat part of the efficiency equation are you trying to solve for? Type \"out\" \"in\", or \"eff\""))
+num = str(input("EFF = (Eout/Ein) * 100%\n\nWhat part of the efficiency equation are you trying\nto solve for? Type \"out\" \"in\", or \"eff\" "))
 
 if (num=="in"):
-    input ("To calculate the ENERGY IN, provide the EFFICIENCY and the and the ENERGY OUT.\nPRESS ENTER") 
-    Eout = float(input("Enter the energy you want to get out, in Joules: "))
-    Eff = float(input("Enter the efficiency, in percent form percent."))
+    print("")
+    input ("To calculate the ENERGY IN, provide the EFFICIENCY\nand the ENERGY OUT.\nPRESS ENTER ") 
+    print("")
+    Eout = float(input("Enter the energy you want to get out, in Joules: \nEout= "))
+    Eff = float(input("Enter the efficiency, in percent form: \nEff= "))
     Ein = Eout/(Eff/100)
+    print("")
     print ("In order to get " + str(Eout) + " J out,")
     print ("with an efficiency of " + str(Eff) + "%,")
     print ("the amount of energy you will") 
@@ -15,10 +18,13 @@ if (num=="in"):
     print (str(Ein) + " Joules.")
 
 if (num=="out"):
-    input ("To calculate the ENERGY OUT, provide the EFFICIENCY and the ENERGY IN.\nPRESS ENTER") 
-    Ein = float(input("Enter the energy that goes in, in Joules: "))
-    Eff = float(input("Enter the efficiency, in percent form."))
+    print("")
+    input ("To calculate the ENERGY OUT, provide the EFFICIENCY\nand the ENERGY IN.\nPRESS ENTER") 
+    print("")
+    Ein = float(input("Enter the energy that goes in, in Joules:\nEin= "))
+    Eff = float(input("Enter the efficiency, in percent form.\n%EFF= "))
     Eout = Ein*(Eff/100)
+    print("")
     print ("If you put " + str(Ein) + " J in,")
     print ("with an efficiency of " + str(Eff) + "%,")
     print ("the amount of energy you will") 
@@ -26,14 +32,15 @@ if (num=="out"):
     print (str(Eout) + " Joules.")
 
 if (num=="eff"):
-    input ("To calculate the EFFICIENCY, provide the ENERGY OUT and the ENERGY IN.\nPRESS ENTER") 
-    Ein = float(input("Enter the energy that goes in, in Joules: "))
-    Eout = float(input("Enter the energy out, in Joules."))
+    print("")
+    input ("To calculate the EFFICIENCY, provide the ENERGY OUT\nand the ENERGY IN.\nPRESS ENTER") 
+    print("")
+    Ein = float(input("Enter the energy that goes in, in Joules:\nEin= "))
+    Eout = float(input("Enter the energy out, in Joules.\nEout= "))
     Eff = ((Eout/Ein)*100)
-    print ("If you put " + str(Ein) + " J in,")
-    print ("and you get out " + str(Eout) + " J,")
+    print ("If you put " + str(Ein) + " Joules in,")
+    print ("and you get out " + str(Eout) + " Joules,")
     print ("The efficiency is " + str(Eff) + "%.")
 
-print
-print ("Have a good day!")
-
+print (" ")
+print ("Have a good day!") 
